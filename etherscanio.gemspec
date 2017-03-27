e0950fbbe59afb44338a0fb18a1a22c3ed709af2
Sebastian Schürmann <sebs@2xs.org>
2017-03-27 21:53:26 +0200

    Spec helper set up

--- etherscanio.gemspec
+++ etherscanio.gemspec
@@ -24,5 +24,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'guard-rspec'
   spec.add_development_dependency 'rubocop'
   spec.add_development_dependency 'guard-rubocop'
+  spec.add_development_dependency 'webmock'
 
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'bundler/setup'
+require 'webmock/rspec'
+require './lib/etherscanio/api'
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+  config.before(:each) do
+    # stub_request(:get, error500)
+    # .to_return(status: 500, body: '', headers: {})
+  end
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
