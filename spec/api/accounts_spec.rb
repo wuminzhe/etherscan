c74d008cfb0d143d4a31134339372d39f469ac62
Sebastian Schürmann <sebs@2xs.org>
2017-03-30 17:06:38 +0200

    Balance

--- spec/api/accounts_spec.rb
+++ spec/api/accounts_spec.rb
@@ -10,7 +10,7 @@ describe Etherscanio::Api do
       let(:address) { 'ox123' }
       let(:tag) { 'foo' }
       it { is_expected.to be_a(Object) }
-      it { is_expected.to eq('https://api.etherscan.io/api?module=account&action=balance&address=ox123&tag=foo') }
+      it { is_expected.to eq('https://api.etherscan.io/api?module=account&action=balance&address=ox123&tag=foo') } 
     end
   end
 end
